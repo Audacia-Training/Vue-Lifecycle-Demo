6e133b494e9794206760c545b9c551e6070483dd
Audacia-Training <112955432+Audacia-Training@users.noreply.github.com>
2022-09-06 14:44:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-uabwmb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-uabwmb)
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { onCreated, onMounted, onUnmounted, ref } from 'vue';
+
+let state = ref('Not Initialized');
+let x = ref(0);
+let y = ref(0);
+
+onMounted(() => {
+  state.value = 'Mounted';
+  window.addEventListener('mousemove', (event: MouseEvent) => {
+    x.value = event.screenX;
+    y.value = event.screenY;
+  });
+});
+
+onUnmounted(() => {
+  state = 'Unmounting';
+});
+</script>
+
+<template>
+  <div>
+    {{ state }}
+  </div>
+  <div>X: {{ x }}</div>
+  <div>Y: {{ y }}</div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
